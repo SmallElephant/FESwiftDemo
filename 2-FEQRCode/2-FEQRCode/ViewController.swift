053b9d9150982cda076c38aeb5f43844b7978f7d
FlyElephant <keso_xiang@talkmate.com>
2016-12-16 15:49:38 +0800

    QRCode~

---
+++ 2-FEQRCode/2-FEQRCode/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  2-FEQRCode
+//
+//  Created by FlyElephant on 16/12/16.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+import UIKit
+//http://adad184.com/2015/09/30/goodbye-zxing/
+//https://github.com/adad184/DemoQRCode
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 2-FEQRCode/2-FEQRCodeTests/__FEQRCodeTests.swift
@@ -0,0 +1,36 @@
+//
+//  __FEQRCodeTests.swift
+//  2-FEQRCodeTests
+//
+//  Created by FlyElephant on 16/12/16.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+import XCTest
+@testable import __FEQRCode
+
+class __FEQRCodeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
